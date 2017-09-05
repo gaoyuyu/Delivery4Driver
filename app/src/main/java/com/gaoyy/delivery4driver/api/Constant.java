57dfc162f09fad8e325d011b8ed0f78e37d38bc2
gaoyuyu <740514999@qq.com>
2017-09-05 22:37:11 +0800

    update api base url

--- app/src/main/java/com/gaoyy/delivery4driver/api/Constant.java
+++ app/src/main/java/com/gaoyy/delivery4driver/api/Constant.java
@@ -3,7 +3,8 @@ package com.gaoyy.delivery4driver.api;
 public class Constant
 {
     public static final String TAG="Sam Delivery";
-    public static final String API_BASE = "http://47.89.184.140/order/";
+    public static final String API_BASE = "http://www.menualliance.com/jeeplus25/";
+//    public static final String API_BASE = "http://47.89.184.140/order/";
 //    public static final String API_BASE = "http://192.168.1.49:8080/order/";
 
     public static final String GOOGLE_MAP_API_BASE = "https://maps.googleapis.com/maps/api/";
